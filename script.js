4fe0d1f49037c1f86e834e1450831ee64362b6ab
Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>
2022-09-03 00:14:15 +0200

    adding some cards in the start of the app. then the user can click

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ import generateCard from "./generateCard.js";
 document.querySelector("#btn").addEventListener("click", () => {
   generateCard();
 });
+
+for(let i=0; i<5; i++) {
+  generateCard()
+}
